e02add165a0af556c56584a33a5f741743653d37
Marlon oliveira barbosa <marlon.academi@hotmail.com>
2026-07-16 11:28:29 -0300

    Fix date comparison in Make.php

--- src/Make.php
+++ src/Make.php
@@ -602,7 +602,7 @@ class Make
         $dateTxtDhEMi = new \DateTime($std->dhEmi);
 
         //mudança privalia
-        if ($dateNow < $dateTxt){
+        if ($dateNow < dateTxtDhEMi){
 
               $std->dhEmi = date('c');
 
